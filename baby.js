c4df90bf31af174fa637ed330b544f060e73abda
郭磊 <6646667@gmail.com>
2023-06-09 09:20:24 +0800

    更新baby.js为正确内容

--- baby.js
+++ baby.js
@@ -1,14 +1,6 @@
-Java.perform(function () {
-    let SettingsColor4 = Java.use("com.nighp.babytracker_android.activities.SettingsColor4");
-    SettingsColor4["onCreateView"].implementation = function (layoutInflater, viewGroup, bundle) {
-        console.log(`SettingsColor4.onCreateView is called: layoutInflater=${layoutInflater}, viewGroup=${viewGroup}, bundle=${bundle}`);
-        let result = this["onCreateView"](layoutInflater, viewGroup, bundle);
-        console.log(`SettingsColor4.onCreateView result=${result}`);
-        return result;
-    };
-
-    SettingsColor4["onScale"].implementation = function (f) {
-        console.log(`SettingsColor4.onScale is called: f=${f}`);
-        this["onScale"](f);
-    };
-});
+XposedHelpers.findAndHookMethod(XposedHelpers.findClass('com.nighp.babytracker_android.activities.MainActivity4', runtime.classLoader),
+    'loadAd', XC_MethodReplacement({
+        replaceHookedMethod: function (param) {
+            return null;
+        }
+    }));
