0c9ddd2c8e5f646ae9c77860b998bcc84abeac28
郭磊 <6646667@gmail.com>
2023-06-09 09:38:02 +0800

    更新

--- babyFrida.js
+++ babyFrida.js
@@ -1,6 +1,10 @@
-Java.perform(function () {
-    let activity = Java.use('com.nighp.babytracker_android.activities.MainActivity4');
-    activity.loadAd.implementation = function () {
-        console.log("hook成功frida")
-    }
-})
+function hookBabyAd() {
+    Java.perform(function () {
+        let activity = Java.use('com.nighp.babytracker_android.activities.MainActivity4');
+        activity.loadAd.implementation = function () {
+            console.log("hook成功frida")
+        }
+    })
+}
+
+hookBabyAd()
